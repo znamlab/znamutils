8bbe22d21146095be3ddcbec21fb129e886abbbc
Antonin Blot <antonin.blot@crick.ac.uk>
2023-07-12 13:11:38 +0100

    [formatting] black

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='znamutils',
-    version='v0.1',
+    name="znamutils",
+    version="v0.1",
     packages=find_packages(),
-    url='https://github.com/znamlab/znamutils',
-    license='MIT',
-    author='Antonin Blot, Petr Znamenskyi',
-    author_email='antonin.blot@crick.ac.uk',
-    description='Common utility functions for analysis',
-    install_requires=[
-        "decopatch",
-        "makefun",
-        "decorator"
-    ],
+    url="https://github.com/znamlab/znamutils",
+    license="MIT",
+    author="Antonin Blot, Petr Znamenskyi",
+    author_email="antonin.blot@crick.ac.uk",
+    description="Common utility functions for analysis",
+    install_requires=["decopatch", "makefun", "decorator"],
 )
